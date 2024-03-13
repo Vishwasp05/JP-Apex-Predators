8f822ff87af4771dff3754392dd556c5454c42e1
Vishwas Sharma <ch0nky@protonmail.com>
2024-03-13 14:38:27 +0530

    Initial Commit

---
+++ JP-Apex-Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP-Apex-Predators
+//
+//  Created by Vishwas Sharma on 13/03/24.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
