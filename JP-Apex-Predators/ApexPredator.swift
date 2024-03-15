c7a9fa7b40cf5c4762d6c162a78e31f857cb7f2b
Vishwas Sharma <ch0nky@protonmail.com>
2024-03-15 21:11:14 +0530

    main-screen-done

---
+++ JP-Apex-Predators/ApexPredator.swift
@@ -0,0 +1,8 @@
+//
+//  ApexPredator.swift
+//  JP-Apex-Predators
+//
+//  Created by Vishwas Sharma on 13/03/24.
+//
+
+import Foundation
---
+++ JP-Apex-Predators/Predators.swift
@@ -0,0 +1,8 @@
+//
+//  Predators.swift
+//  JP-Apex-Predators
+//
+//  Created by Vishwas Sharma on 13/03/24.
+//
+
+import Foundation
